36602d72c9fe0fc9ebffcbede44afd433dfb3dda
khira9504 <k.hiraizumi0417@gmail.com>
2025-01-14 15:51:58 +0900

    修正

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import { ViteEjsPlugin } from "vite-plugin-ejs";
-import appendHeadScriptsAtBody from "./appendHeadScriptsAtBody";
+// import appendHeadScriptsAtBody from "./appendHeadScriptsAtBody";
 
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
   if (command === 'serve') {
@@ -10,6 +10,7 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
       root: './src',
       build: {
         outDir: '../dist',
+        emptyOutDir: true,
       },
       plugins: [
         ViteEjsPlugin(),
@@ -21,7 +22,30 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
       root: './src',
       build: {
         outDir: '../dist',
+        emptyOutDir: true,
+        assetsInlineLimit: 0,
         rollupOptions: {
+          output: {
+            assetFileNames: (assetInfo) => {
+              console.log(assetInfo);
+              
+              let extType = assetInfo.name.split('.')[1];
+              //Webフォントファイルの振り分け
+              if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
+                extType = 'fonts';
+              }
+              if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                extType = 'images';
+              }
+              //ビルド時のCSS名を明記してコントロールする
+              if(extType === 'css') {
+                return `assets/css/style.css`;
+              }
+              return `assets/${extType}/[name][extname]`;
+            },
+            chunkFileNames: 'assets/js/[name].js',
+            entryFileNames: 'assets/js/[name].js',
+          },
           input: {
             main: resolve(__dirname, 'src/index.html'),
             page: resolve(__dirname, 'src/page.html'),
@@ -29,7 +53,7 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
         },
       },
       plugins: [
-        appendHeadScriptsAtBody(),
+        // appendHeadScriptsAtBody(),
         ViteEjsPlugin(),
       ],
     }
