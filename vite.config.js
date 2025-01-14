434beaf393bd6180a85e898fc3d0128faa14fa00
khira9504 <k.hiraizumi0417@gmail.com>
2025-01-14 16:07:04 +0900

    修正

--- vite.config.js
+++ vite.config.js
@@ -27,17 +27,13 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
         rollupOptions: {
           output: {
             assetFileNames: (assetInfo) => {
-              console.log(assetInfo);
-              
               let extType = assetInfo.name.split('.')[1];
-              //Webフォントファイルの振り分け
               if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
                 extType = 'fonts';
               }
               if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
                 extType = 'images';
               }
-              //ビルド時のCSS名を明記してコントロールする
               if(extType === 'css') {
                 return `assets/css/style.css`;
               }
